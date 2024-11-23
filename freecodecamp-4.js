72b20ca71e39ef9e505e4cd919908546ce8da161
Irlan05 <irlanf06@gmail.com>
2024-11-23 12:29:34 -0300

    Add files via upload

---
+++ freecodecamp-4.js
@@ -0,0 +1,32 @@
+function redirecionar1(){
+    const select=document.getElementById('guitar1')
+    const url= select.value
+    if(url !==""){
+        window.open(url, "_blank")
+    }
+    else {
+        alert('Por favor selecione uma opção!')
+    }
+}
+
+function redirecionar2(){
+    const select=document.getElementById('guitar2')
+    const url= select.value
+    if(url !==""){
+        window.open(url, "_blank")
+    }
+    else {
+        alert('Por favor selecione uma opção!')
+    }
+}
+
+function redirecionar3(){
+    const select=document.getElementById('guitar3')
+    const url= select.value
+    if(url !==""){
+        window.open(url, "_blank")
+    }
+    else {
+        alert('Por favor selecione uma opção!')
+    }
+}
